29ea3e6974ec845af1e14e8fce6eba6ca3200a4b
Norman Matrix <norman@normanmatrix.xyz>
2020-06-24 10:02:39 +0200

    initial commit - pretested

---
+++ gameoftrackers.py
@@ -0,0 +1,350 @@
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+import networkx as nx
+from enum import Enum
+from typing import Tuple
+from collections import deque
+import random
+import math
+import uuid
+
+# in 10th of seconds
+SIMULATION_RUN_STEPS = 9000
+MAC_ROTATE_STEPS_TOTAL = 9000
+RPI_ROTATE_STEPS_MIN = 6000
+RPI_ROTATE_STEPS_MAX = 9000
+ADVERTISE_MIN = 2
+ADVERTISE_MAX = 3 # 2.7 according to spec
+ADVERTISE_GALAXY_S10_MIN = 30
+ADVERTISE_GALAXY_S10_MAX = 50
+
+STEP_RESOLUTION_MS = 100
+
+# number of steps for 1 square map movement
+MOVEMENT_STEPS_PER_SQUARE = 10
+
+class ExposureNotificationTimerTypes(Enum):
+    """Timer setup according to Google/Apple specifications or specific Android test"""
+    EXPOSURE_NOTIFICATION_TIMER_SPEC = 1
+    EXPOSURE_NOTIFICATION_TIMER_ANDROID = 2
+    EXPOSURE_NOTIFICATION_TIMER_ANDROID_R = 3
+    EXPOSURE_NOTIFICATION_TIMER_ANDROID_R_FIXED = 4
+
+
+class Map:
+    
+    """Map with NxN square fields where agent moves around.
+       Agent switches position with agent already inhibiting target square."""
+    def __init__(self, cluster_size, side_length):
+        self._map = [[None for i in range(side_length)] for j in range(side_length)]
+        self._cluster_size = cluster_size
+        self._side_length = side_length
+        self._placed = 0
+        self._step = 0
+
+
+    def place_agent(self, agent_id):
+        """Place agent on random free square"""
+        assert self._placed < (self._side_length * self._side_length)
+
+        x = y = None
+        while x is None and y is None:
+            x = random.randint(0, self._side_length)
+            y = random.randint(0, self._side_length)
+
+            if self._map[x,y] is None:
+                self._placed += 1
+            else:
+                x = y = None
+
+    def spatially_unique(self, agent1_id, agent2_id):
+        """Check if two agents are spatially unique (at least one square apart)"""
+
+        a1x, a1y = self.locate_agent(agent1_id)
+        a2x, a2y = self.locate_agent(agent2_id)
+
+        return abs(a1x - a2x) >= 2 or abs(a1y - a2y) >= 2
+
+    def locate_agent(self, agent_id):
+        """Locate agent by ID"""
+
+        for x in range(self._side_length):
+            for y in range(self._side_length):
+                if self._map[x,y] == agent_id:
+                    return x, y
+
+        raise ValueError(f"unable to locate agent {agent_id}")
+
+    def step(self):
+        """Agents move in horizontal or diagonal direction for 1 square every movement steps"""
+
+        agent_ids = set([a for r in self._map for a in r if not a is None])
+        agent_slice = MOVEMENT_STEPS_PER_SQUARE / self._cluster_size
+        for agent_id in agent_ids:
+            agent_offset = math.floor(agent_slice * agent_id)
+            if (self._step + agent_offset) % MOVEMENT_STEPS_PER_SQUARE == 0:
+                x, y = self.locate_agent(agent_id)
+                dx, dy = random.randrange(-1, 2), random.randrange(-1, 2)
+
+                if self._map[x + dx, y + dy] is None:
+                    self._map[x, y] = None
+                else:
+                    self._map[x, y] = self._map[x + dx, y + dy]
+
+                self._map[x + dx, y + dy] = agent_id
+
+        self._step += 1
+
+
+TimeSeriesDatum = Tuple[int, str, str, int, int]
+
+class TimeSeriesData:
+    """Captures all simulation events for post processing"""
+    def __init__(self):
+        self.data = []
+    
+    def add(self, datum: TimeSeriesDatum):
+        """Add record to time series database"""
+        self.data += [datum]
+
+    def trackable_fraction(self, cluster_size, advertising_interval):
+        """Calculate trackable devices in collected data"""
+        g = nx.DiGraph()
+        traversal_lut_mac = {} # lookup rotations with known identifier directly carrying over
+        traversal_lut_rpi = {}
+        lost_tracks = 0
+
+        temporal_lookahead = deque() # look ahead one advertising_interval to see if this tuple is
+                                        # unique at once - at that point it can be considered a unique
+                                        # rotation indirectly carrying over
+
+        for step, mac, rpi, x, y in self.data + [(99999999, None, None)]:
+            if not (mac, rpi) is (None, None): # skip processing for last rerun to cleanup deque
+
+                if (mac, rpi) not in g.nodes.items():
+                    g.add_node((mac, rpi))
+
+                # direct traversals via rpi
+                if mac in traversal_lut_mac:
+
+                    prev_rpi = traversal_lut_mac[mac]
+                    del traversal_lut_mac[mac]
+                    del traversal_lut_rpi[prev_rpi]
+
+                    g.add_edge((mac, prev_rpi), (mac, rpi))
+
+                # direct traversals via mac
+                elif rpi in traversal_lut_rpi:
+
+                    prev_mac = traversal_lut_rpi[rpi]
+                    del traversal_lut_mac[prev_mac]
+                    del traversal_lut_rpi[rpi]
+
+                # indirect lookup via temporal rotation uniqueness
+                else:
+                    temporal_lookahead.append((step, mac, rpi, x, y))
+
+            shift_n = 0
+
+            # process temporal lookahead for current step
+            while len(temporal_lookahead) >= 2 and \
+                    (step - temporal_lookahead[0][0]) >= (2 * advertising_interval) and \
+                    (step - temporal_lookahead[1][0]) >= advertising_interval:
+
+                recover_src = recover_dst = None               
+                if (temporal_lookahead[1][0] - temporal_lookahead[0][0]) >= advertising_interval:
+                    # temporal uniqueness
+                    recover_src = (temporal_lookahead[0][1], temporal_lookahead[0][2])
+                    recover_dst = (temporal_lookahead[1][1], temporal_lookahead[1][2])
+
+                else:
+                    # spatial uniqueness
+                    x0, y0 = tuple(temporal_lookahead[0][3:5])
+                    x1, y1 = tuple(temporal_lookahead[1][3:5])
+
+                    if abs(x0-x1) >= 2 or abs(y0-y1) >= 2:
+                        recover_src = (temporal_lookahead[0][1], temporal_lookahead[0][2])
+                        recover_dst = (temporal_lookahead[1][1], temporal_lookahead[1][2])
+
+                if not (recover_src is None and recover_dst is None):
+
+                    g.add_edge(recover_src, recover_dst)
+                    temporal_lookahead.popleft()
+                    temporal_lookahead.popleft()
+
+
+            # lost track on oldest stale items if not already used in processing
+            # (must be too narrow to neighbor)
+            while len(temporal_lookahead) >= shift_n and \
+                (step - temporal_lookahead[shift_n][0]) >= (2 * advertising_interval):
+                shift_n += 1
+            
+            for _ in range(shift_n):
+                temporal_lookahead.popleft()
+        
+            lost_tracks += shift_n
+
+            # save lut for following ref cycles
+            traversal_lut_mac[mac] = rpi
+            traversal_lut_rpi[rpi] = mac
+        
+        if len(g.edges) == 0:
+            return 1.0
+        else:
+            return 1.0 - (cluster_size - lost_tracks) / len(g.edges) # 0 edges XXXXX ??
+
+
+class ExposureNotificationTimers:
+    """Timer model for Google/Apple ExposureNotification specs"""
+    def __init__(self, timer_type: ExposureNotificationTimerTypes, guid: int, time_series_data: TimeSeriesData, space_map: Map):
+        self._time_series_data = time_series_data
+        self._guid = guid
+        self._step = 0
+        self.is_fixed = False
+        self._space_map = space_map
+        self._rotate_mac()
+        self._rotate_rpi()
+
+        {
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_SPEC: ExposureNotificationTimers._gen_EXPOSURE_NOTIFICATION_TIMER_SPEC,
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID: ExposureNotificationTimers._gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID,
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID_R: ExposureNotificationTimers._gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID_R,
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID_R_FIXED: ExposureNotificationTimers._gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID_R_FIXED
+        }[timer_type](self)
+
+    def step(self):
+        """Execute simulation step"""
+        if self._step % self.mac_rotate_step == 0:
+            self._rotate_mac()
+        if self._step % self.rpi_rotate_step == 0:
+            self._rotate_rpi()
+        if self._step % self.advertise_step == 0:
+            self._advertise()
+        
+        self._step += 1
+
+    def _gen_EXPOSURE_NOTIFICATION_TIMER_SPEC(self):
+        """Generate timers according to Google/Apple spec"""
+        self.mac_rotate_step = random.randrange(0, MAC_ROTATE_STEPS_TOTAL + 1)
+        self.rpi_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.advertise_step = random.randrange(ADVERTISE_MIN, ADVERTISE_MAX + 1)
+
+    def _gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID(self):
+        """Generate timers according to observed Android behavior"""
+        self.mac_rotate_step = random.randrange(0, MAC_ROTATE_STEPS_TOTAL + 1)
+        self.rpi_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.advertise_step = random.randrange(ADVERTISE_GALAXY_S10_MIN, ADVERTISE_GALAXY_S10_MAX + 1)
+
+    def _gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID_R(self):
+        """Generate timers according to reported Android R behavior"""
+        self.mac_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.rpi_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.advertise_step = random.randrange(ADVERTISE_GALAXY_S10_MIN, ADVERTISE_GALAXY_S10_MAX + 1)
+
+    def _gen_EXPOSURE_NOTIFICATION_TIMER_ANDROID_R_FIXED(self):
+        """Generate timers according to reported Android R behavior with dual rotation fixed"""
+        self.mac_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.rpi_rotate_step = random.randrange(RPI_ROTATE_STEPS_MIN, RPI_ROTATE_STEPS_MAX + 1)
+        self.advertise_step = random.randrange(ADVERTISE_GALAXY_S10_MIN, ADVERTISE_GALAXY_S10_MAX + 1)
+        self.is_fixed = True
+
+    def _rotate_mac(self):
+        """Rotate MAC address"""
+        self._mac = uuid.uuid4()
+        if self.is_fixed: # fix for dual rotation not taking place currently in this direction
+            self._rpi = uuid.uuid4()
+
+    def _rotate_rpi(self):
+        """Rotate rolling proximity identifier"""
+        self._rpi = uuid.uuid4()
+        self._mac = uuid.uuid4()
+
+    def _advertise(self):
+        x, y = self._space_map.locate_agent(self._guid)
+        self._time_series_data.add((self.step, self._mac, self._rpi, x, y))
+
+
+class DeviceOwnerAgent(Agent):
+    """An agent carrying an ExposureNotification enabled Apple/Google device"""
+    def __init__(self, unique_id, model, timers):
+        super().__init__(unique_id, model)
+        self._timers = timers
+
+    def step(self):
+        """Execute agent step"""
+        self._timers.step()
+
+
+class DeviceClusterModel(Model):
+    """A model with a single or multiple beacons observing a device cluster of given size"""
+    def __init__(self, cluster_size: int, timer_type: ExposureNotificationTimerTypes, triangulated, time_series_data: TimeSeriesData, space_map: Map):
+        self.num_agents = cluster_size
+        self.schedule = RandomActivation(self)
+        self.schedule.add(space_map) # map needs step activation for movement
+        for i in range(self.num_agents):
+            space_map.place_agent(i)
+            timers = ExposureNotificationTimers(timer_type, guid=i, time_series_data=time_series_data, space_map=space_map)
+            a = DeviceOwnerAgent(i, model=self, timers=timers)
+            self.schedule.add(a)
+
+
+def run_game(n_simulation_runs: int, cluster_size: int, triangulated: bool, timer_type: ExposureNotificationTimerTypes):
+    """Run game with parameters, returns trackable fraction of devices per run"""
+    assert n_simulation_runs > 0
+    assert cluster_size > 0
+
+    trackables = []
+
+    if timer_type == ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_SPEC:
+        
+        advertising_interval = ADVERTISE_MAX
+
+    elif timer_type in [
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID,
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID_R,
+            ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_ANDROID_R_FIXED
+        ]:
+
+        advertising_interval = ADVERTISE_GALAXY_S10_MAX
+    
+    else:
+        assert False
+
+    space_map = Map(cluster_size, cluster_size + 1)
+    model = None
+    time_series_data = None
+    for _ in range(0, n_simulation_runs):
+        time_series_data = TimeSeriesData()
+        model = DeviceClusterModel(cluster_size,
+                                timer_type=timer_type,
+                                triangulated=triangulated,
+                                time_series_data=time_series_data,
+                                space_map=space_map)
+
+        for _ in range(0, SIMULATION_RUN_STEPS):
+            model.step()
+            space_map.step()
+
+        trackables += [time_series_data.trackable_fraction(cluster_size=cluster_size,
+                                                           advertising_interval=advertising_interval)]
+
+    return trackables
+
+def render_game(n_frames: int, frame_time_steps: int, cluster_size: int, triangulated: bool, timer_type: ExposureNotificationTimerTypes):
+    """Render game with parameters, returns trackable fraction of devices per run"""
+
+    #  https://eli.thegreenplace.net/2016/drawing-animated-gifs-with-matplotlib/
+    #
+    # non-spatial (globals)
+    #
+    # d1  d2  d3
+    #-d4--d5- d6
+    #
+    #
+    # spatial (geo-buckets)
+    # 
+    # +--+--+--+--+
+    # |d1|d2|d3|d4|
+    # +  +  +  +  +
+    # |  |  |  |d6|    movement = move/loc switch
+    # +--+--+--+--+
---
+++ test_got.py
@@ -0,0 +1,87 @@
+import uuid
+import math
+from gameoftrackers import TimeSeriesData, ExposureNotificationTimers, ExposureNotificationTimerTypes, SIMULATION_RUN_STEPS, DeviceOwnerAgent, run_game, Map
+
+def test_got_TimeSeriesData():
+
+    sx0 = 0
+    macx0 = uuid.uuid4()
+    rpix0 = uuid.uuid4()
+
+    sy0 = 2
+    macy0 = uuid.uuid4()
+    rpiy0 = uuid.uuid4()
+
+    sz0 = 6    
+    macz0 = uuid.uuid4()
+    rpiz0 = uuid.uuid4()
+
+    sx1 = 10
+    macx1 = macx0
+    rpix1 = uuid.uuid4()
+    
+    sy1 = 12
+    macy1 = uuid.uuid4()
+    rpiy1 = rpiy0
+    
+    sz1 = 16
+    macz1 = uuid.uuid4() # full rotation at 16
+    rpiz1 = uuid.uuid4()
+
+    sx2 = 20
+    macx2 = uuid.uuid4() # full rotation at 20
+    rpix2 = uuid.uuid4()
+    
+    sy2 = 22
+    macy2 = macy1
+    rpiy2 = uuid.uuid4()
+    
+    sz2 = 26
+    macz2 = uuid.uuid4()
+    rpiz2 = rpiz1
+
+    t = TimeSeriesData()
+    t.add((sx0, macx0, rpix0, -1, -1))
+    t.add((sy0, macy0, rpiy0, -1, -1))
+    t.add((sz0, macz0, rpiz0, -1, -1))
+    t.add((sx1, macx1, rpix1, -1, -1))
+    t.add((sy1, macy1, rpiy1, -1, -1))
+    t.add((sz1, macz1, rpiz1, -1, -1))
+    t.add((sx2, macx2, rpix2, -1, -1))
+    t.add((sy2, macy2, rpiy2, -1, -1))
+    t.add((sz2, macz2, rpiz2, -1, -1))
+
+    fraction = t.trackable_fraction(3, 10)
+
+    assert fraction >= .66665 and fraction <= .66667
+
+def test_got_ExposureNotificationTimers():
+
+    space_map = Map(1, 2)
+    time_series_data = TimeSeriesData()
+    timers = ExposureNotificationTimers(ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_SPEC, guid=0, time_series_data=time_series_data, space_map=space_map)
+
+    for _ in range(SIMULATION_RUN_STEPS):
+        timers.step()
+
+    MIN_ADVERTISINGS = math.floor(SIMULATION_RUN_STEPS / 3)
+    MAX_ADVERTISINGS = math.floor(SIMULATION_RUN_STEPS / 2)
+    l = len(time_series_data.data)
+    assert l >= MIN_ADVERTISINGS and l <= MAX_ADVERTISINGS
+
+def test_got_run_game():
+    trackables = run_game(30, 3, False, ExposureNotificationTimerTypes.EXPOSURE_NOTIFICATION_TIMER_SPEC)
+    assert len(trackables) == 30
+    assert trackables[0] >= .99999
+
+def test_got_render_game():
+    pass
+
+def test_got_enriched_graph():
+    pass
+
+def test_deep_got():
+    # supervised training on identified cases
+    # monte carlo? score adjustments?
+    # just spec
+    pass
